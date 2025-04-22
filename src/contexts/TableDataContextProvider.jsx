076b7399043a7c0550feb4fdb59f284ed03dab99
jhayphix <jhayphix05@gmail.com>
2025-04-22 17:34:34 +0100

    Work on external scholarship

--- src/contexts/TableDataContextProvider.jsx
+++ src/contexts/TableDataContextProvider.jsx
@@ -152,12 +152,12 @@ const TableDataContextProvider = ({ children }) => {
       console.log("filteredData: ", filteredData);
 
       const applicationTableData = filteredData?.map((application) => {
-        // Flatten the responses into a single object with a "QQ" indicator in the label
+        // Flatten the responses into a single object with a "Form" indicator in the label
         const responses = application?.responseSections?.reduce(
           (acc, section) => {
             section.responses?.forEach((response) => {
-              // Adding "QQ" to the label (you could change "QQ" to any other appropriate term)
-              const labelWithIndicator = `QQ ${response.label}`;
+              // Adding "Form" to the label (you could change "Form" to any other appropriate term)
+              const labelWithIndicator = `Response: ${response.label}`;
 
               // Add the formatted label and response to the accumulator
               acc[labelWithIndicator] = Array.isArray(response.response)
