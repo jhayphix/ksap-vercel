439ebee5d5439945319aba4f59fbe51475971a3a
jhayphix <jhayphix05@gmail.com>
2025-04-22 13:36:53 +0100

    Work on external scholarship

--- src/components/cards/ExternalScholarshipHeaderCard.jsx
+++ src/components/cards/ExternalScholarshipHeaderCard.jsx
@@ -1,41 +1,41 @@
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { FaEdit } from "react-icons/fa";
-import { MdDelete } from "react-icons/md";
+import { useContext, useEffect } from "react";
 
 import DefaultBadge from "@components/tags/DefaultBadge";
 import { ConfigContext } from "@contexts/ConfigContextProvider";
-import { NavigationContext } from "@contexts/NavigationContextProvider";
-
-import DropdownWrapper from "@components/dropdown/DropdownWrapper";
 import { AuthContext } from "@contexts/AuthContextProvider";
-import { ExternalScholarshipContext } from "@contexts/ExternalScholarshipContextProvider";
+import { UserContext } from "@contexts/UserContextProvider";
+
+import ExternalScholarshipActionBtn from "@components/buttons/ExternalScholarshipActionBtn";
 
 const ScholarshipHeaderCard = ({ className, externalScholarshipData = {} }) => {
   const { HELPER } = useContext(ConfigContext);
-  const { updateScholarshipRoute } = useContext(NavigationContext);
   const { authStatus } = useContext(AuthContext);
-  const { showDeleteExternalScholarshipModal } = useContext(
-    ExternalScholarshipContext
-  );
+  const { loadAdmins, adminStatus, getAdminReturn } = useContext(UserContext);
+
+  // Load admins
+  useEffect(() => {
+    loadAdmins();
+  }, [loadAdmins]);
+  const adminsData = adminStatus?.adminsFlattened;
 
   const scholarshipId = externalScholarshipData?.id;
-  const scholarshipCreatedAt = externalScholarshipData?.createdAt;
-  const scholarshipCreatedByAdminId = externalScholarshipData?.createdByAdminId;
-  const scholarshipDeadline = externalScholarshipData?.deadline;
-  const scholarshipIsActive = externalScholarshipData?.isActive;
-  const scholarshipLastUpdatedAt = externalScholarshipData?.lastUpdatedAt;
-  const scholarshipLastUpdatedByAdminId =
-    externalScholarshipData?.lastUpdatedByAdminId;
   const scholarshipName = externalScholarshipData?.name;
   const scholarshipSponsor = externalScholarshipData?.sponsor;
   const scholarshipUrl = externalScholarshipData?.url;
 
+  const scholarshipDeadline = externalScholarshipData?.deadline;
   const scholarshipIsDue = new Date(scholarshipDeadline) <= new Date();
+  const scholarshipIsActive = externalScholarshipData?.isActive;
 
-  const handleDeleteScholarship = () => {
-    showDeleteExternalScholarshipModal(scholarshipId, scholarshipName);
-  };
+  const scholarshipCreatedAt = externalScholarshipData?.createdAt;
+  const scholarshipCreatedByAdminId = externalScholarshipData?.createdByAdminId;
+  const scholarshipCreatedBy =
+    getAdminReturn(adminsData, scholarshipCreatedByAdminId) || {};
+  const scholarshipLastUpdatedAt = externalScholarshipData?.lastUpdatedAt;
+  const scholarshipLastUpdatedByAdminId =
+    externalScholarshipData?.lastUpdatedByAdminId;
+  const scholarshipLastUpdatedBy =
+    getAdminReturn(adminsData, scholarshipLastUpdatedByAdminId) || {};
 
   const scholarshipDetailsObj = [
     {
@@ -88,7 +88,7 @@ const ScholarshipHeaderCard = ({ className, externalScholarshipData = {} }) => {
       sectionDetails: [
         {
           label: "Published By",
-          value: scholarshipCreatedByAdminId,
+          value: scholarshipCreatedBy?.fullName || "N/A",
         },
         {
           label: "Published At",
@@ -96,7 +96,7 @@ const ScholarshipHeaderCard = ({ className, externalScholarshipData = {} }) => {
         },
         {
           label: "Last Updated By",
-          value: scholarshipLastUpdatedByAdminId,
+          value: scholarshipLastUpdatedBy?.fullName || "N/A",
         },
         {
           label: "Last Updated At",
@@ -125,26 +125,12 @@ const ScholarshipHeaderCard = ({ className, externalScholarshipData = {} }) => {
       </div>
 
       {authStatus?.isUserSuperAdmin ? (
-        <DropdownWrapper
-          id="scholarshipHeaderCardDropdown"
-          className="bg_secondary_3 rounded"
-        >
-          <Link
-            role="button"
-            to={updateScholarshipRoute?.getPath(scholarshipId)}
-            className="btn dropdown-item"
-          >
-            <FaEdit size={20} className="me-2" /> Edit
-          </Link>
-
-          <hr className="my-3" />
-          <button
-            className="btn dropdown-item text_danger"
-            onClick={() => handleDeleteScholarship()}
-          >
-            <MdDelete size={20} className="me-2" /> Delete
-          </button>
-        </DropdownWrapper>
+        <>
+          <ExternalScholarshipActionBtn
+            scholarshipId={scholarshipId}
+            scholarshipName={scholarshipName}
+          />
+        </>
       ) : (
         ""
       )}
