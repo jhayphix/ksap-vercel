afd9e46b0e37347e40e608f4fcae2149d4d0cfd7
jhayphix <jhayphix05@gmail.com>
2025-04-25 14:23:18 +0100

    Minor Update

--- src/components/tables/tableTools/TablePagination.jsx
+++ src/components/tables/tableTools/TablePagination.jsx
@@ -29,69 +29,74 @@ const TablePagination = ({
 
   return (
     <div
-      className="d-flex justify-content-between align-items-center has_scrollbar mt-3"
+      className="d-flex flex-md-row flex-column  justify-content-between align-items-center has_scrollbar mt-3"
       style={{ overflowX: "auto", maxWidth: "100vw" }}
     >
-      {canDownload ? (
-        <CSVLink
-          data={filteredDataWithoutId}
-          filename={`${tableName}.csv`}
-          className="btn btn_secondary_2 d-none d-md-inline-block"
-        >
-          <FaDownload /> Download Table
-        </CSVLink>
-      ) : (
-        <div></div>
-      )}
+      <div className="align-self-start mb-md-0 mb-4">
+        {canDownload ? (
+          <CSVLink
+            data={filteredDataWithoutId}
+            filename={`${tableName}.csv`}
+            // className="btn btn_secondary_2 d-none d-md-inline-block"
+            className="btn btn_secondary_2"
+          >
+            <FaDownload /> Download Table
+          </CSVLink>
+        ) : (
+          <div></div>
+        )}
+      </div>
 
-      <div className="d-flex justify-content-end align-items-center">
-        <label className="me-2 fw-light d-none d-md-inline-block">
-          Rows per page:
-        </label>
-        <select
-          value={rowsPerPage}
-          onChange={(e) => setRowsPerPage(Number(e.target.value))}
-          className="form-select me-3"
-          style={{ width: "80px" }}
-        >
-          <option value="10">10</option>
-          <option value="25">25</option>
-          <option value="50">50</option>
-          <option value="100">100</option>
-        </select>
-        <span className="me-3">
-          {indexOfFirstRow + 1} -{" "}
-          {Math.min(indexOfLastRow, filteredData.length)} of{" "}
-          {filteredData.length}
-        </span>
-        <button
-          className="btn btn-light mx-1"
-          onClick={() => setCurrentPage(1)}
-          disabled={currentPage === 1}
-        >
-          <FaAngleDoubleLeft />
-        </button>
-        <button
-          className="btn btn-light mx-1"
-          onClick={() => setCurrentPage(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          <FaAngleLeft />
-        </button>
-        <button
-          className="btn btn-light mx-1"
-          onClick={() => setCurrentPage(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          <FaAngleRight />
-        </button>
-        <button
-          className="btn btn-light mx-1"
-          onClick={() => setCurrentPage(totalPages)}
-          disabled={currentPage === totalPages}
-        >
-          <FaAngleDoubleRight />
-        </button>
+      <div className="align-self-end">
+        <div className="d-flex justify-content-end align-items-center">
+          <label className="me-2 fw-light d-none d-md-inline-block">
+            Rows per page:
+          </label>
+          <select
+            value={rowsPerPage}
+            onChange={(e) => setRowsPerPage(Number(e.target.value))}
+            className="form-select me-3"
+            style={{ width: "80px" }}
+          >
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+          <span className="me-3">
+            {indexOfFirstRow + 1} -{" "}
+            {Math.min(indexOfLastRow, filteredData.length)} of{" "}
+            {filteredData.length}
+          </span>
+          <button
+            className="btn btn-light mx-1"
+            onClick={() => setCurrentPage(1)}
+            disabled={currentPage === 1}
+          >
+            <FaAngleDoubleLeft />
+          </button>
+          <button
+            className="btn btn-light mx-1"
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            <FaAngleLeft />
+          </button>
+          <button
+            className="btn btn-light mx-1"
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            <FaAngleRight />
+          </button>
+          <button
+            className="btn btn-light mx-1"
+            onClick={() => setCurrentPage(totalPages)}
+            disabled={currentPage === totalPages}
+          >
+            <FaAngleDoubleRight />
+          </button>
+        </div>
       </div>
     </div>
   );
