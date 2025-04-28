9292fdc354cff433d921796460ea3b1f80473057
jhayphix <jhayphix05@gmail.com>
2025-04-28 23:25:40 +0100

    Work on custom api changes

--- src/api/exportAPIService.js
+++ src/api/exportAPIService.js
@@ -3,12 +3,12 @@ import * as API_CUSTOM_SERVER_SERVICES from "@src/api/apiCustom";
 import * as API_FIREBASE_SERVICES from "@src/api/apiFireBase";
 
 const SERVICES_API = {
-  "json-server": API_JSON_SERVER_SERVICES,
+  jsonServer: API_JSON_SERVER_SERVICES,
   custom: API_CUSTOM_SERVER_SERVICES,
   firebase: API_FIREBASE_SERVICES,
 };
 
-const ACTIVE_SERVICE = "custom";
+const ACTIVE_SERVICE = "firebase";
 
 const APIService = SERVICES_API[ACTIVE_SERVICE] || API_JSON_SERVER_SERVICES;
 
